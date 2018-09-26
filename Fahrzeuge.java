9ca92a7f9f0ea8cdf7e947fc1de1993f937cb57c
Davon Maurer <hballertx@gmail.com>
2018-09-26 10:43:55 +0200

    fd

--- Fahrzeuge.java
+++ Fahrzeuge.java
@@ -7,27 +7,7 @@
  */
 public class Fahrzeuge
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Fahrzeuge
-     */
-    public Fahrzeuge()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(inasdfasdft y)
-    {
-        // put your code here
-        return x + y;
-    }
+   public Fahrzeuge(){
+    
+   }
 }
