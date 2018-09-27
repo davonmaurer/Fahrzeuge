e85dd6ce8e89f25c3b7606162d14e9d82615654b
damau <damau@LAPTOP-O6IFVQVS.mshome.net>
2018-09-27 11:38:00 +0200

    v

--- Auto.java
+++ Auto.java
@@ -7,27 +7,12 @@
  */
 public class Auto extends Fahrzeuge
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Auto
-     */
-    public Auto()
+   private int personen;
+   
+    public Auto(int personen)
     {
-        // initialise instance variables
-        x = 0;
+       this.personen=personen;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
